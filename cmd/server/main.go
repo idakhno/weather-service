0adbd93afcaf7f8eb5d92e18254782273292584f
Ilya Dakhno <ilya.dakhno@gmail.com>
2025-10-19 14:56:03 +0300

    added base logic for cron jobs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
 )
 
+// Server constants
 const httpPort = ":3000"
 
 func main() {
@@ -24,8 +29,56 @@ func main() {
 		}
 	})
 
-	// Start listening at desired port
-	if err := http.ListenAndServe("httpPort", r); err != nil {
+	// Init cron jobs
+	s, err := gocron.NewScheduler()
+	if err != nil {
 		log.Print(err)
 	}
+
+	jobs, err := initJobs(s)
+	if err != nil {
+		log.Print(err)
+	}
+
+	// Init wait groups for multithreading jobs
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	// Start listening at desired port, non-blocking
+	go func() {
+		defer wg.Done()
+		fmt.Println("starting server on port " + httpPort)
+		if err := http.ListenAndServe(httpPort, r); err != nil {
+			log.Print(err)
+		}
+	}()
+
+	// Start cron jobs
+	go func() {
+		defer wg.Done()
+		fmt.Println("starting job:", jobs[0].ID())
+		s.Start()
+	}()
+
+	// Waiting for all multithreading jobs to be completed
+	wg.Wait()
+}
+
+// Initialize cron jobs
+func initJobs(scheduler gocron.Scheduler) ([]gocron.Job, error) {
+	j, err := scheduler.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("hello world")
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return []gocron.Job{j}, nil
 }
